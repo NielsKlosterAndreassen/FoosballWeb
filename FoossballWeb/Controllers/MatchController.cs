bef7b2518a9de753ffa9bece7ea3b6d66abc0519
Niels Kloster Andreassen <nika@telenor.dk>
2013-03-08 15:56:10 +0100

    Added rest create match

---
+++ FoossballWeb/Controllers/MatchController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Ankiro.Framework.Tools.Bus;
+using FoossballPlayars.Commands;
+using FoossballPlayars.QueryContext;
+
+namespace FoossballWeb.Controllers
+{
+	public class MatchController : Controller
+	{
+		private readonly IBus _commandBus;
+		private readonly IScoreQuery _scoreQuery;
+
+		public MatchController(IBus commandBus, IScoreQuery scoreQuery)
+		{
+			_commandBus = commandBus;
+			_scoreQuery = scoreQuery;
+		}
+
+		public JsonResult Create(string redOffensive, string redDefensive, string blueOffensive, string blueDefensive, int scoreRed, int scoreBlue)
+		{
+			var players = _scoreQuery.GetAllPlayers().ToDictionary(x=>x.Name.ToString(), x=>x.Id);
+
+			_commandBus.Raise(new PlayGameCommand(players[redOffensive], players[redDefensive], players[blueOffensive], players[blueDefensive], scoreRed, scoreBlue));
+
+			return Json("Ok");
+		}
+	}
+}
